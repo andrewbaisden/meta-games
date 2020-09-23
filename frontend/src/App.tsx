07b7ff3a82559665dbd1ead4440be63237b837e4
Andrew Baisden <andrewbaisden@gmail.com>
2020-09-23 17:49:12 +0100

    renamed venv to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,10 @@ const App: React.FC = () => {
 	}, []);
 
 	const getAPI = () => {
-		const API = 'http://127.0.0.1:5000/';
+		// Online Version
+		const API = 'https://backend-meta-games-5lfwtew5f.vercel.app/';
+		// Local Version
+		// const API = 'http://127.0.0.1:5000/';
 
 		fetch(API)
 			.then((response) => {
