235b4fc2c9106363103025f0ff53ff1018a9fdd7
Andrew Baisden <andrewbaisden@gmail.com>
2020-09-23 16:10:12 +0100

    style and design improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,18 @@ import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
 
 interface Game {
-	developer: String;
-	game_name: String;
-	genre: String;
-	id: Number;
-	img_url: String;
-	meta_score: Number;
-	platforms: String;
-	rating: String;
-	release_year: Number;
-	summary: String;
-	__createdtime: Number;
-	__updatedtime__: Number;
+	developer: string;
+	game_name: string;
+	genre: string;
+	id: number;
+	img_url: string;
+	meta_score: number;
+	platforms: string;
+	rating: string;
+	release_year: number;
+	summary: string;
+	__createdtime: number;
+	__updatedtime__: number;
 }
 
 const App: React.FC = () => {
@@ -45,17 +45,57 @@ const App: React.FC = () => {
 
 	return (
 		<Fragment>
-			<div>
+			<header>
+				<h1>Meta Game Reviews</h1>
+			</header>
+			<main>
 				{loading === false ? (
-					<div>Loading...</div>
-				) : (
 					<div>
-						{gameData.map((game) => (
-							<div key={String(game.id)}>{game.id}</div>
-						))}
+						<h1>Loading...</h1>
 					</div>
+				) : (
+					<section>
+						{gameData.map((game) => {
+							let metaColor = 'low';
+
+							if (game.meta_score >= 70) {
+								metaColor = 'high';
+							} else if (game.meta_score <= 69 && game.meta_score >= 49) {
+								metaColor = 'medium';
+							} else {
+								metaColor = 'low';
+							}
+
+							return (
+								<div className="game-container" key={String(game.id)}>
+									<h1>{game.game_name}</h1>
+									<p>
+										<strong>Developer:</strong> {game.developer}
+									</p>
+									<p>
+										<strong>Genre:</strong> {game.genre}
+									</p>
+									<img src={game.img_url} alt={game.game_name} />
+
+									<p>
+										<strong>Meta Score:</strong> <span className={metaColor}>{game.meta_score}</span>
+									</p>
+									<p>
+										<strong>Available on:</strong> {game.platforms}
+									</p>
+									<p>
+										<strong>Rating:</strong> {game.rating}
+									</p>
+									<p>
+										<strong>Release Year:</strong> {game.release_year}
+									</p>
+									<p>{game.summary}</p>
+								</div>
+							);
+						})}
+					</section>
 				)}
-			</div>
+			</main>
 		</Fragment>
 	);
 };
